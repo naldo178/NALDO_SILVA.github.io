2c5f6cc87ec29790f3dc3b06ca8604479eb3e365
JOSINALDO DA SILVA <87744405+naldo178@users.noreply.github.com>
2024-02-11 17:48:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,19 @@
+const hour = document.querySelector('#hour')
+const min = document.querySelector('#min')
+const sec = document.querySelector('#sec')
 
-/* EJECUTA LA FUNCIÓN AL CARGAR */
-onload = ondea; 
+setInterval(() => {
+  let date = new Date()
+  let dHour = date.getHours()
+  let dMinute = date.getMinutes()
+  let dSec = date.getSeconds()
 
-var anchoFaja = 2; //ANCHO EN PIXELES DE CADA FAJA EN EL CLIP
-var anchoOnda = 25; //VALOR DISTANCIA (DEMORA) EN SENOIDE
+  hour.innerHTML = `${formatTime(dHour)}`
+  min.innerHTML = `${formatTime(dMinute)}`
+  sec.innerHTML = `${formatTime(dSec)}`
 
-function ondea(){
-  /* REFIERE AL TÍTULO */ 
-  var titulo = document.querySelector("h1"); 
-  /* CAPTURA EL CONTENIDO DEL TÍTULO */
-  var texto = titulo.innerHTML; 
-  /* MIDE EL TAMAÑO DEL TÍTULO */
-  var ancho = titulo.offsetWidth; 
-  var alto = titulo.offsetHeight; 
+}, 1000)
 
-  /*SIEMPRE AJUSTA EL ancho COMO MÚLTIPLO DE anchoFaja EN PIXELES*/
-  ancho = ancho+(anchoFaja-ancho%anchoFaja); 
-  /* FIJA EL TAMAÑO DEL TÍTULO */
-  titulo.style.width = ancho+"px"; 
-  titulo.style.height = alto+"px"; 
-
-  /* LA CANTIDAD DE BANDAS ES EL ANCHO DE TÍTULO SOBRE ANCHO DE CLIP */
-  var totalFajas = ancho/2; 
-
-  /* VACÍA EL TÍTULO CONTENEDOR DE TEXTO */
-  titulo.innerHTML = ""; 
-
-  /* CREA LAS BANDAS Y LES DA FORMATO */
-  for(i=0; i<totalFajas; i++) {
-    /* UN DIV PARA CADA FAJA */
-    faja = document.createElement("div"); 
-    /* LE ASIGNA LA MISMA ALTURA DEL TÍTULO */
-    faja.style.height = alto+"px"; 
-    /* PONE EL MISMO TEXTO ORIGINAL */
-    faja.innerHTML = texto; 
-    /* DEJA VISIBLE UN CLIP DE 2px DE ANCHO, CADA UNO 2px A LA IZQUIERDA DEL ANTERIOR PARA QUE PAREZCA UNA IMAGEN DE TÍTULO COMPLETA SIN CORTES */
-    faja.style.clip = "rect(0, "+((i+1)*anchoFaja)+"px, "+alto+"px, "+(i*anchoFaja)+"px)"; 
-    /* RETRASA LA ANIMACIÓN CSS DE CADA FAJA SIGUIENDO UNA ONDA DE TIEMPO SENOIDE */
-    faja.style.animationDelay = (Math.cos(i)+i*anchoOnda)+"ms"; 
-    /* AGREGA LA CAPA AL CONTENEDOR */
-    titulo.appendChild(faja);
-  }
+function formatTime(time) {
+  return time < 10 ? '0' + time : time
 }
